8fc79b4c4f41ce9bd396fccff0f503826d4e1ff3
kornumic <kornumic@gmail.com>
2023-07-28 14:59:42 +0200

    Added current/next page loading

--- src/components/pages/Characters.tsx
+++ src/components/pages/Characters.tsx
@@ -3,48 +3,88 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import CharacterItem from "../character/CharacterItem";
 
+type PageList = {
+  prev: string | null;
+  next: string | null;
+};
+
 const CharactersPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
   const [fetchedCharacters, setFetchedCharacters] = useState<Character[]>([]);
+  const [pages, setPages] = useState<PageList>({ prev: null, next: null });
+  const [currentPage, setCurrentPage] = useState<string>(
+    "https://rickandmortyapi.com/api/character",
+  );
 
   useEffect(() => {
     async function fetchCharacters() {
       setIsLoading(true);
 
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const response = await fetch(currentPage);
       if (!response.ok) {
         setError(true);
       } else {
         const responseJson = await response.json();
-        const results: Character[] = responseJson.results;
-        setFetchedCharacters(results);
+        const characters: Character[] = responseJson.results;
+        const pages: PageList = {
+          prev: responseJson.info.prev,
+          next: responseJson.info.next,
+        };
+        setFetchedCharacters(characters);
+        setPages(pages);
       }
 
       setIsLoading(false);
     }
 
     fetchCharacters().then(() => {});
-  }, []);
+  }, [currentPage]);
+
+  const prevButtonHandler = () => {
+    if (pages.prev) {
+      setCurrentPage(pages.prev);
+    } else {
+      throw new Error("Previous page is not defined");
+    }
+  };
+
+  const nextButtonHandler = () => {
+    if (pages.next) {
+      setCurrentPage(pages.next);
+    } else {
+      throw new Error("Next page is not defined");
+    }
+  };
 
   return (
-    <div>
-      {isLoading && !error && <p>Loading...</p>}
-      {error && !isLoading && <p>Unexpected error</p>}
-      {!error && !isLoading && (
-        <ul className="flex-col">
-          {fetchedCharacters.map((character) => {
-            return (
-              <li className="text-center" key={character.id}>
-                <Link to={`${character.id}`}>
-                  <CharacterItem character={character} />
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
+    <>
+      <div>
+        {isLoading && !error && <p>Loading...</p>}
+        {error && !isLoading && <p>Unexpected error</p>}
+        {!error && !isLoading && (
+          <ul className="flex-col">
+            {fetchedCharacters.map((character) => {
+              return (
+                <li className="text-center" key={character.id}>
+                  <Link to={`${character.id}`}>
+                    <CharacterItem character={character} />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+      <div>
+        <button disabled={!pages.prev} onClick={prevButtonHandler}>
+          Previous
+        </button>
+        <button disabled={!pages.next} onClick={nextButtonHandler}>
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
