79d1730e190e238341fcff715be7698f26f1e5be
kornumic <kornumic@gmail.com>
2023-08-03 01:07:50 +0200

    Added basic episode items loaded from api

--- src/components/episode/EpisodeItem.tsx
+++ src/components/episode/EpisodeItem.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Episode } from "./EpisodeInfo";
+import CharacterMinimalList from "../character/CharacterMinimalList";
 
 const EpisodeItem: React.FC<{ episode: Episode }> = ({ episode }) => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col rounded-2xl h-64 items-top bg-[#181818] hover:bg-[#303030] border-white transition-all m-4 p-4 hover:underline underline-offset-0 hover:underline-offset-4">
+      <div>
+        <h1 className="text-2xl transition-all">{episode.name}</h1>
+      </div>
+      <div className="flex justify-end items-end h-full">
+        <CharacterMinimalList />
+      </div>
+    </div>
+  );
 };
 
 export default EpisodeItem;
--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { PageList } from "./Characters";
+import { Episode } from "../components/episode/EpisodeInfo";
+import useUrlState from "@ahooksjs/use-url-state";
+import useHttp from "../hooks/use-http";
+import PagesChanger from "../components/layout/PagesChanger";
+import EpisodeItem from "../components/episode/EpisodeItem";
+import { Link } from "react-router-dom";
 
 const EpisodesPage = () => {
+  const [fetchedEpisodes, setFetchedEpisodes] = useState<Episode[]>([]);
+  const [pages, setPages] = useState<PageList>({ prev: false, next: false });
+  const [currentPage, setCurrentPage] = useUrlState({ page: 1 });
+  const episodesUrl = "https://rickandmortyapi.com/api/episode";
+
+  const { isLoading, error, sendRequest } = useHttp();
+
+  useEffect(() => {
+    async function applyData(data: any) {
+      const episodes: Episode[] = data.results;
+      const pages: PageList = {
+        prev: !!data.info.prev,
+        next: !!data.info.next,
+      };
+      setFetchedEpisodes(episodes);
+      setPages(pages);
+    }
+
+    sendRequest(
+      { url: episodesUrl + "/?page=" + currentPage.page.toString() },
+      applyData,
+    ).then();
+  }, [sendRequest, currentPage]);
+
+  const pageButtonHandler = (move: number) => {
+    if (move === -1 && pages.prev) {
+      setCurrentPage({ page: +currentPage.page + move });
+    } else if (move === 1 && pages.next) {
+      setCurrentPage({ page: +currentPage.page + move });
+    } else {
+      throw new Error("Page is not defined");
+    }
+  };
+
   return (
-    <div className="flex justify-center h-screen items-center">
-      <h1 className="text-4xl text-center">Episodes</h1>
-    </div>
+    <>
+      {isLoading && !error && <p className="transition-all">Loading...</p>}
+      {error && !isLoading && <p>Unexpected error</p>}
+      {!error && !isLoading && (
+        <div className="my-6 mx-64">
+          <PagesChanger
+            pages={pages}
+            prevButtonHandler={pageButtonHandler.bind(null, -1)}
+            nextButtonHandler={pageButtonHandler.bind(null, 1)}
+          />
+          <ul className="grid grid-cols-4  ">
+            {fetchedEpisodes.map((episode) => {
+              return (
+                <li key={episode.id}>
+                  <Link to={`${episode.id}`}>
+                    <EpisodeItem episode={episode} />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+
+          <PagesChanger
+            pages={pages}
+            prevButtonHandler={pageButtonHandler.bind(null, -1)}
+            nextButtonHandler={pageButtonHandler.bind(null, 1)}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
