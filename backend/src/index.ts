3ecb6aab0be72659ccbfb6590fd2abb504a3aea4
kornumic <kornumic@gmail.com>
2023-09-19 00:07:59 +0200

    Used mongoose to connect to db, loading config from .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import * as mongoose from "mongoose";
 
-//For env File
-dotenv.config();
+dotenv.config({ path: __dirname + "/../.env" });
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
@@ -11,6 +11,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
-});
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`;
+console.log(uri);
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is Fire at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
