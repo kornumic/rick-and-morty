5967cde27f1fd40c63f7d85606d53390024b40ad
kornumic <kornumic@gmail.com>
2023-09-21 22:38:39 +0200

    Switched to sequelize, added postgres with docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,35 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
-import * as mongoose from "mongoose";
+import { Sequelize } from "sequelize";
 
 dotenv.config({ path: __dirname + "/../.env" });
-const { MONGO_USER, MONGO_PASSWORD, MONGO_CLUSTER } = process.env;
+
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_URL } = process.env;
+if (!POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_URL)
+  throw new Error("Missing env variables");
+
+const sequelize = new Sequelize(
+  `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_URL}`,
+);
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
 });
 
-mongoose
-  .connect(
-    `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}?retryWrites=true&w=majority`,
-  )
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is Fire at http://localhost:${port}`);
+const run = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+
+    app.listen(PORT, () => {
+      console.log(`Server is Fire at http://localhost:${PORT}`);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+run().then();
