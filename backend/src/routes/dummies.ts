340082be3946244dcef43c2cd4f8b0334ab07ca4
kornumic <kornumic@gmail.com>
2023-09-22 16:47:22 +0200

    Dummy routes for locations

--- backend/src/routes/dummies.ts
+++ backend/src/routes/dummies.ts
@@ -1,5 +1,6 @@
 import { Character } from "./character-routes";
 import { Episode } from "./episode-routes";
+import { Location } from "./location-routes";
 
 export const DUMMY_CHARACTERS: Character[] = [
   {
@@ -12,6 +13,11 @@ export const DUMMY_CHARACTERS: Character[] = [
     origin: 1,
     location: 3,
     image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    episode: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+    ],
     created: "2017-11-04T18:48:46.250Z",
   },
   {
@@ -24,6 +30,11 @@ export const DUMMY_CHARACTERS: Character[] = [
     origin: undefined,
     location: 3,
     image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    episode: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+    ],
     created: "2017-11-04T18:50:21.651Z",
   },
 ];
@@ -39,5 +50,19 @@ export const DUMMY_EPISODES: Episode[] = [
       394, 395, 435,
     ],
     created: "2017-11-10T12:56:33.798Z",
+  },
+];
+
+export const DUMMY_LOCATIONS: Location[] = [
+  {
+    id: 1,
+    name: "Earth (C-137)",
+    type: "Planet",
+    dimension: "Dimension C-137",
+    residents: [
+      38, 45, 71, 82, 83, 92, 112, 114, 116, 117, 120, 127, 155, 169, 175, 179,
+      186, 201, 216, 239, 271, 302, 303, 338, 343, 356, 394,
+    ],
+    created: "2017-11-10T12:42:04.162Z",
   },
 ];
--- backend/src/routes/location-routes.ts
+++ backend/src/routes/location-routes.ts
@@ -1,9 +1,58 @@
 import express from "express";
+import { DUMMY_LOCATIONS } from "./dummies";
 
 const router = express.Router();
 
+export type Location = {
+  id: number | undefined;
+  name: string;
+  type: string;
+  dimension: string;
+  residents: number[];
+  created: string;
+};
+
+// GET /api/location/:locationId
+router.get("/:locationId", (req, res) => {
+  console.log(req.params.locationId);
+  const location = DUMMY_LOCATIONS.find((c) => c.id === +req.params.locationId);
+  console.log(location);
+  if (!location) {
+    return res.status(404).json({ message: "Location not found" });
+  }
+  res.json(location);
+});
+
+// POST /api/location
+router.post("/", (req, res) => {
+  const location = req.body as Location;
+  location.id = DUMMY_LOCATIONS.length + 1;
+  DUMMY_LOCATIONS.push(location);
+
+  res.json({ location });
+});
+
+// GET /api/location?page=1&perPage=20
 router.get("/", (req, res) => {
-  res.json({ message: "location works" });
+  const page = +(req.query.page || "1");
+  const locationsPerPage = +(req.query.perPage || "20");
+  const offset = (page - 1) * locationsPerPage;
+
+  const locations = DUMMY_LOCATIONS.slice(offset, offset + locationsPerPage);
+
+  res.json({
+    info: {
+      count: DUMMY_LOCATIONS.length,
+      pages: Math.ceil(DUMMY_LOCATIONS.length / locationsPerPage),
+      next:
+        page + 1 <= Math.ceil(DUMMY_LOCATIONS.length / locationsPerPage)
+          ? page + 1
+          : undefined,
+      prev: page - 1 > 0 ? page - 1 : undefined,
+    },
+    results: locations,
+  });
+  res.status(200);
 });
 
 export default router;
